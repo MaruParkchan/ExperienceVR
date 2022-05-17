d8bd8de55e2c0c9e891d9c3def5a963a148953b1
MaruParkchan <36887125+MaruParkchan@users.noreply.github.com>
2022-05-18 01:20:15 +0900

    완성작

---
+++ Assets/#Script/ScreenShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+#if UNITY_EDITOR
+    public int num;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            num++;
+            ScreenCapture.CaptureScreenshot("ScreenShot" + num + ".png");
+            Debug.Log("스크린샷저장");
+        }
+    }
+#endif
+}
